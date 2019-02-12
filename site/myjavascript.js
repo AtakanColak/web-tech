b36e8c1ae0aa566d38479725a8ecfbbc466c2b44
Atakan Colak <atakancolak9897@gmail.com>
2019-02-12 08:52:59 +0000

    JS

---
+++ site/myjavascript.js
@@ -0,0 +1,35 @@
+"use strict";
+
+//strings.js (it has error)
+//var s = "x";
+//s.name = "my string";
+//s.toUpperCase();
+//var c = 'x';
+//console.log(s, typeof s, s.name, typeof c, c==s,c===s);
+
+//numbers.js (there is no float or int, THERE IS ONLY NUMBER)
+//var i = 42;
+//var f = 42.24465;
+//console.log(typeof i, typeof f, Math.floor(f), i.toFixed(2), f.toFixed(2));
+
+//arrays.js
+// var a = [4,5,6];
+// console.log(typeof a, a[1]);
+// a.push(7);
+// console.log(a);
+// a = [];
+// console.log(a ==[]);
+// console.log(a.length ==0);
+
+//bool.js
+// console.log(typeof true);
+// console.log(0 ? true : false);
+// console.log(0 == false, 0 === false);
+
+//read.js
+// process.stdin.setEncoding('utf8');
+// process.stdin.on('data', receive);
+
+// function receive(text) {
+//     console.log("You typed ", text);
+// }
