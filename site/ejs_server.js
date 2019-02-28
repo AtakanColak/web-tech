42a85aa61a025ed90d7dee102b7641c5a5fe60fd
Atakan Colak <atakancolak9897@gmail.com>
2019-02-28 21:30:49 +0000

    EJS Var Echo Intro

--- site/ejs_server.js
+++ site/ejs_server.js
@@ -5,7 +5,7 @@ var app = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-
+app.use( express.static( "public" ) );
 // use res.render to load up an ejs view file
 
 // index page 
@@ -13,10 +13,6 @@ app.get('/', function(req, res) {
     res.render('pages/index');
 });
 
-// // about page 
-// app.get('/about', function(req, res) {
-//     res.render('pages/about');
-// });
 
 app.listen(8080);
 console.log('8080 is the magic port');
