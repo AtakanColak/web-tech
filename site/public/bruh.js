2afa90b09b96cb12f04df0b4e47ac179be687985
Anton Sukhorukov <as16989@my.bristol.ac.uk>
2019-02-28 19:33:12 +0000

    removal of node modules from git

--- site/public/bruh.js
+++ site/public/bruh.js
@@ -11,12 +11,12 @@ var sql = require("sqlite3");
 var db = new sql.Database("bruh.db");
 db.serialize(create);
 function create() {
-    db.run("CREATE TABLE Release (ID INTEGER NOT NULL PRIMARY KEY, AlbumArtPath Text, ReleaseName Text, ArtistID int, RelType int, RelDate Date, ReleaseLength int, LabelID int, RelFormat int, Rating float, Bio Text, NumRatings int, GenreID int);");
+    db.run("CREATE TABLE Release (ID INTEGER NOT NULL PRIMARY KEY, AlbumArtPath Text, ReleaseName Text, ArtistID int, RelType int2, RelDate Date, ReleaseLength int, LabelID int, RelFormat int, Rating float, Bio Text, NumRatings int, GenreID int2);");
     db.run("CREATE TABLE Genre (ID INTEGER NOT NULL PRIMARY KEY, GenreName Str, ParentGenreID int);");
     db.run("CREATE TABLE Track (ID INTEGER NOT NULL PRIMARY KEY, TrackName Str, TrackLength Time, TrackPath Str, ReleaseID int, TrackIndex int);");
     db.run("CREATE TABLE Review (ID INTEGER NOT NULL PRIMARY KEY, ReleaseID int, UserID int, Rating float, Comment Str);");
     db.run("CREATE TABLE ShoppingItem (ID INTEGER NOT NULL PRIMARY KEY, ReleaseID int, CatalogNum int, Price decimal, RelFormat int);");
     db.run("CREATE TABLE OrderItem (ID INTEGER NOT NULL PRIMARY KEY, UserID int, ShoppingItemID int);");
 
-    db.run("INSERT INTO Release VALUES(NULL, 'images/cover.png', 'Naked Flames Who Can Recall His Past Lives', 389, 4, 2018-01-01, 50, 31, 123, 3.4, 'Lorem Ipsum Dolor Sit Amet', 21, 2)");
+    db.run("INSERT INTO Release VALUES(NULL, 'images/cover.png', 'Naked Flames Who Can Recall His Past Lives', 389, 0001, 2018-01-01, 50, 31, 123, 3.4, 'Lorem Ipsum Dolor Sit Amet', 21, 00001)");
 }
