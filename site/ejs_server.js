181939a5c4401a1e4056ceeb9fdc82dfeaf78e2b
Anton Sukhorukov <as16989@my.bristol.ac.uk>
2019-02-28 21:33:33 +0000

    queries

--- site/ejs_server.js
+++ site/ejs_server.js
@@ -13,6 +13,21 @@ app.get('/', function(req, res) {
     res.render('pages/index');
 });
 
+var sql = require("sqlite3");
+var db = new sql.Database("/home/anton/Documents/ataweb/web-tech/site/public/bruh.db");
+
+let sqlQuery = `SELECT ReleaseName relNam FROM Release`;
+
+var testVar = 'populate me';
+
+db.each(sqlQuery, (err, row) => {
+    if (err) {
+        throw err;
+    }
+    testVar = `${row.relNam}`;
+    console.log(testVar);
+});
+
 // // about page 
 // app.get('/about', function(req, res) {
 //     res.render('pages/about');
--- site/public/bruh.js
+++ site/public/bruh.js
@@ -1,22 +1,20 @@
-// "use strict";
-// var sql = require("sqlite3");
-// var util = require('util');
-// var exec = require('child_process').exec;
-// function puts(error, stdout, stderr) { console.log(stdout) }
-
-// exec("sqlite3 {/home/anton/Documents/bruh.db} < {release.sql}", puts);
-
 "use strict";
 var sql = require("sqlite3");
 var db = new sql.Database("bruh.db");
 db.serialize(create);
 function create() {
-    db.run("CREATE TABLE Release (ID INTEGER NOT NULL PRIMARY KEY, AlbumArtPath Text, ReleaseName Text, ArtistID int, RelType int2, RelDate Date, ReleaseLength int, LabelID int, RelFormat int, Rating float, Bio Text, NumRatings int, GenreID int2);");
+    db.run("CREATE TABLE Release (ID INTEGER NOT NULL PRIMARY KEY, AlbumArtPath Text, ReleaseName Text, ArtistID int, RelType int, RelDate Date, ReleaseLength Text, LabelID int, RelFormat int2, Rating float, Bio Text, NumRatings int, GenreID int2);");
     db.run("CREATE TABLE Genre (ID INTEGER NOT NULL PRIMARY KEY, GenreName Str, ParentGenreID int);");
     db.run("CREATE TABLE Track (ID INTEGER NOT NULL PRIMARY KEY, TrackName Str, TrackLength Time, TrackPath Str, ReleaseID int, TrackIndex int);");
     db.run("CREATE TABLE Review (ID INTEGER NOT NULL PRIMARY KEY, ReleaseID int, UserID int, Rating float, Comment Str);");
     db.run("CREATE TABLE ShoppingItem (ID INTEGER NOT NULL PRIMARY KEY, ReleaseID int, CatalogNum int, Price decimal, RelFormat int);");
     db.run("CREATE TABLE OrderItem (ID INTEGER NOT NULL PRIMARY KEY, UserID int, ShoppingItemID int);");
 
-    db.run("INSERT INTO Release VALUES(NULL, 'images/cover.png', 'Naked Flames Who Can Recall His Past Lives', 389, 0001, 2018-01-01, 50, 31, 123, 3.4, 'Lorem Ipsum Dolor Sit Amet', 21, 00001)");
+    db.run("INSERT INTO Release VALUES(NULL, 'images/cover.png', 'Naked Flames Who Can Recall His Past Lives', 389, 4, 2018-01-01, time(12146, 'unixepoch'), 31, 1111, 3.4, 'Lorem Ipsum Dolor Sit Amet', 21, 01011)");
+
+    db.all("select * from Release", show);
+    function show(err, rows) {
+        if (err) throw err;
+        console.log(rows);
+    }
 }
