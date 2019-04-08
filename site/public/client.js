7215217c3cced224d147ad0d81c287453fd2630f
Atakan Colak <atakancolak9897@gmail.com>
2019-04-08 16:58:49 +0100

    End of 08/04/2019

--- site/public/client.js
+++ site/public/client.js
@@ -19,10 +19,11 @@ $(function () {
         e.preventDefault();
         var name = $(this).parents(".jumbotron").find("#input_song_name").val();
         var length = $(this).parents(".jumbotron").find("#input_song_length").val();
-        $(this).text(length);
+        var path = $(this).parents(".jumbotron").find("#input_track_path").val();
+        var index = $(this).parents(".jumbotron").find("#input_song_number").val();
         var isValidTime = /^\d\d:([01]?[0-9]|2[0-3]):[0-5][0-9]$/.test(length);
-        if (name != "" & isValidTime) {
-            $(this).parents(".song-section").find(".song-list ul").append("<li class=\"row\">" + name + "</li>");
+        if (name != "" && isValidTime && path != "" && index > 0) {
+            $(this).parents(".song-section").find(".song-list ul").append("<li class=\"row\"><h5 class=\"col-md-2 text-center\">"+index + ".</h5><h5 class=\"col-md-6 text-center\">"+ name + "</h5><h5 class=\"col-md-2 text-center\">" + length  + "</h5></li>");
         }
         else {
             $(this).parents(".jumbotron").find(".invalid-song").prop("hidden", false);
