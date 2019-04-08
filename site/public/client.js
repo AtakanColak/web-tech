c28cc081897390adb1bf0e52fb9426e63ed1dc10
Atakan Colak <atakancolak9897@gmail.com>
2019-04-08 12:06:29 +0100

    Checkboxes

--- site/public/client.js
+++ site/public/client.js
@@ -1,9 +1,23 @@
-$(function(){
+$(function () {
 
-    $(".dropdown-menu a").click(function(){
-        // $(this).parents(".form-group").hide();
-    $(this).parents(".form-group").find(".dropdown-toggle").text($(this).text());
-    $(this).parents(".form-group").find(".dropdown-toggle").val($(this).index());
-   });
+    $(".dropdown-menu a").click(function () {
+        $(this).parents(".form-group").find(".dropdown-toggle").text($(this).text());
+        $(this).parents(".form-group").find(".dropdown-toggle").val($(this).index());
+    });
 
+    $('input[type="file"]').change(function (e) {
+        var fileName = e.target.files[0].name;
+        $('.custom-file-label').html(fileName);
+    });
+
+    $('input[type="checkbox"]').change(function(e) {
+        if ($(this).attr("checked") == "1") {
+            $(this).attr("checked", 0);
+        }
+        else if ($(this).attr("checked") == "0") {
+            $(this).attr("checked", 1);
+        }
+        // $(this).attr("checked", !$(this).attr("checked")); 
+        // $(this).parents("label").val($(this).prop("checked"));
+    });
 });
